f9a1c6c1e0b43103e11350b105d0ae9a2c9ff49e
Benjamin Gray <bengray178@gmail.com>
2019-12-09 11:02:51 +1000

    print tree

--- script/install.js
+++ script/install.js
@@ -2,6 +2,7 @@ const cp = require("child_process");
 const path = require("path");
 
 function install() {
+  cp.execSync("tree", {stdio: "inherit"});
   cp.execSync("autoreconf -vfi", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
   cp.execSync("./configure", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
 }
