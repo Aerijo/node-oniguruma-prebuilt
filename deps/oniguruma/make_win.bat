108faf20811f79a0e6f81664e592484cab0abebc
Benjamin Gray <bengray178@gmail.com>
2019-12-09 14:15:45 +1000

    try this

--- deps/oniguruma/make_win.bat
+++ deps/oniguruma/make_win.bat
@@ -1,3 +1,6 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+
 SET ONIG_DIR=%~dp0\src
 set THIS_DIR=%~dp0
 set BUILD_DIR=%cd%
