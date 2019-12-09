cfb6ba23079be7539b68356af79020503c5743e7
Benjamin Gray <bengray178@gmail.com>
2019-12-09 16:57:50 +1000

    n

--- script/install.js
+++ script/install.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 function install() {
   if (process.platform === "win32") {
-    cp.execSync('"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\Common7\\Tools\\VsDevCmd.bat"', {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
-    cp.execSync("make_win.bat", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
+    cp.execSync('"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\Common7\\Tools\\VsDevCmd.bat" && make_win.bat', {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
+    // cp.execSync("", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
   } else {
     cp.execSync("autoreconf -vfi", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
     cp.execSync("./configure", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
