e6cf450ef36e9b38adc63f620be9e92114165d15
Benjamin Gray <bengray178@gmail.com>
2019-12-09 10:51:50 +1000

    update install script

---
+++ script/install.js
@@ -0,0 +1,9 @@
+const cp = require("child_process");
+const path = require("path");
+
+function install() {
+  cp.execSync("autoreconf -vfi", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
+  cp.execSync("./configure", {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
+}
+
+install();
